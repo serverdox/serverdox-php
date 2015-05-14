5a9290178c803010c4951fdc1dd240fe12ebd563
Liam O'Connor <liam@serverdox.com>
2015-05-14 22:16:49 +0100

    Revert "SDK Initial Commit"
    This reverts commit c0cecba85c7ae6c0b0d30abeb4d5da285a23a527.

--- src/Serverdox/connection/exceptions/InvalidArgumentException.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-namespace Serverdox\Connection\Exceptions;
-
-class InvalidArgumentException extends \Exception{}
--- src/Serverdox/connection/exceptions/InvalidCredentials.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-namespace Serverdox\Connection\Exceptions;
-
-class InvalidCredentials extends \Exception{}
